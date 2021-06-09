df5aeb5f12bb48a6a928185978976038538e9119
Lizator <frederikkoefoeds@gmail.com>
2021-06-09 10:16:29 +0200

    added image sending

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
-from flask import Flask, request
+# !flask/bin/python
+from flask import *
 from flask_restful import Resource, Api
-import turnGen.SolitareChecker as SolitareChecker
-from turnGen.objects.Card import Card
-from turnGen.objects.Pile import Pile
-from turnGen.objects.Gameboard import Gameboard
-import turnGen.AlgoChooser as AlgoChooser
-import turnGen.InstructionConverter as InstructionConverter
-import json
+from datetime import datetime
+from syvkabale.syvkabaleBE.turnGen.objects.Gameboard import Gameboard
+from syvkabale.syvkabaleBE.turnGen.objects.Gameboard import Card
+from syvkabale.syvkabaleBE.turnGen.objects.Gameboard import Pile
+import syvkabale.syvkabaleBE.turnGen.SolitareChecker as SolitareChecker
+import syvkabale.syvkabaleBE.turnGen.InstructionConverter as InstructionConverter
+import syvkabale.syvkabaleBE.turnGen.AlgoChooser as AlgoChooser
+
 
 app = Flask(__name__)
 api = Api(app)
+
 
 def gameboardEncoder(gameboard):
     dict = {
@@ -87,13 +90,38 @@ class TurnGeneration(Resource):
 
 
 class ImgRecon(Resource):
+    def get(self):
+        return render_template('upload.html')
+
     def post(self):
-        b64 = request.get_json()["img"]
-        return gameboardEncoder(Gameboard())
+        now = datetime.now()
+        uploaded_file = request.files['file']
+        if uploaded_file.filename != '':
+            uploaded_file.save(now.strftime("%d_%m_%Y-%H_%M_%S"))
+            return gameboardEncoder(Gameboard()), 200
+        else:
+            return {"Error": True}, 404
 
 
 api.add_resource(TurnGeneration, '/turn/')
-api.add_resource(ImgRecon, '/img/')
+api.add_resource(ImgRecon, '/')
+
+
+"""
+@app.route('/',methods=['GET'])
+def index():
+   return render_template('upload.html')
+
+@app.route('/', methods=['POST'])
+def upload_file():
+    now = datetime.now()
+    uploaded_file = request.files['file']
+    if uploaded_file.filename != '':
+        uploaded_file.save(now.strftime("%d_%m_%Y-%H_%M_%S"))
+        return jsonify("Board in her")
+"""
+
 
 if __name__ == '__main__':
-    app.run(port=8080, debug=True)
+    app.run(host="0.0.0.0", port="5000", debug=True)
+
