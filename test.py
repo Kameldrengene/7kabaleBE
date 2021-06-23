95995df0f245c9d3fefa798ae33a8d4276c40e24
Volkan Isik <s180103@student.dtu.dk>
2021-06-23 18:23:29 +0200

    test rettet

--- test.py
+++ test.py
@@ -1,9 +1,6 @@
 import io
+
 from flask import json
-from io import StringIO
-
-import flask
-
 from app import app
 import unittest
 
@@ -19,9 +16,10 @@ class FlaskTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_picture(self):
-        data =dict(
-            file=(io.BytesIO(b"abcdef"), 'test.jpg')
-        )
+        image = "test.jpg"
+        data = {
+            'file': (open(image, 'rb'), image)
+        }
         tester = app.test_client(self)
         response = tester.post('/',
                                  content_type='multipart/form-data',
@@ -31,9 +29,10 @@ class FlaskTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_board(self):
-        data =dict(
-            file=(io.BytesIO(b"abcdef"), 'test.jpg')
-        )
+        image = "test.jpg"
+        data = {
+            'file': (open(image, 'rb'), image)
+        }
         tester = app.test_client(self)
         response = tester.post('/',
                                  content_type='multipart/form-data',
@@ -42,7 +41,8 @@ class FlaskTestCase(unittest.TestCase):
 
         responsedata = json.loads(response.get_data(as_text=True))
         self.assertEqual(response.status_code, 200)
-        assert responsedata["deckPointer"] == 1
+        print(responsedata)
+        assert responsedata["deckPointer"] == 0
 
     def test_Turn(self):
         data = json.dumps({'deckPointer': 1, 'finSpaceConverter': {'0': 'a', '1': 'b', '2': 'c', '3': 'd'}, 'deck': [{'type': 3, 'value': 10}, {'type': 0, 'value': 14}], 'spaces': {'0': {'shownCards': [{'type': 2, 'value': 4}], 'hiddenCards': []}, '1': {'shownCards': [{'type': 3, 'value': 12}], 'hiddenCards': [{'type': 0, 'value': 14}]}, '2': {'shownCards': [{'type': 0, 'value': 11}], 'hiddenCards': [{'type': 0, 'value': 14}, {'type': 0, 'value': 14}]}, '3': {'shownCards': [{'type': 3, 'value': 6}], 'hiddenCards': [{'type': 0, 'value': 14}, {'type': 0, 'value': 14}, {'type': 0, 'value': 14}]}, '4': {'shownCards': [{'type': 1, 'value': 10}], 'hiddenCards': [{'type': 0, 'value': 14}, {'type': 0, 'value': 14}, {'type': 0, 'value': 14}, {'type': 0, 'value': 14}]}, '5': {'shownCards': [{'type': 0, 'value': 13}], 'hiddenCards': [{'type': 0, 'value': 14}, {'type': 0, 'value': 14}, {'type': 0, 'value': 14}, {'type': 0, 'value': 14}, {'type': 0, 'value': 14}]}, '6': {'shownCards': [{'type': 1, 'value': 3}], 'hiddenCards': [{'type': 0, 'value': 14}, {'type': 0, 'value': 14}, {'type': 0, 'value': 14}, {'type': 0, 'value': 14}, {'type': 0, 'value': 14}, {'type': 0, 'value': 14}]}}, 'finSpaces': {'0': [{'type': 0, 'value': 2}], '1': [{'type': 1, 'value': 1}], '2': [{'type': 2, 'value': 1}], '3': [{'type': 3, 'value': 3}]}})
